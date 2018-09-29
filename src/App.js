4b2f11f27b77f80a84ac99d251cce0931269dead
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-09-29 16:05:01 +0200

    Add components to App.js and setup react-router-dom & redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Movies from './components/movies/Movies';
+import Header from './components/layout/Header';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Movies} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
