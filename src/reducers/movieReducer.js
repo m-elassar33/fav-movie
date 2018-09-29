406b766f9842007bbb2abe02f27f7ca335810616
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-09-29 19:35:06 +0200

    Add the logic for filterMovies to movieReducer

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,7 +1,8 @@
-import { GET_MOVIES, GET_MOVIE } from '../actions/types';
+import { GET_MOVIES, GET_MOVIE, FILTER_MOVIES } from '../actions/types';
 
 const initialState = {
   movies: [],
+  displayedMovies: [],
   movie: {}
 };
 
@@ -10,12 +11,20 @@ export default function(state = initialState, action) {
     case GET_MOVIES:
       return {
         ...state,
-        movies: action.payload
+        movies: action.payload,
+        displayedMovies: action.payload
       };
     case GET_MOVIE:
       return {
         ...state,
         movie: action.payload
+      };
+    case FILTER_MOVIES:
+      return {
+        ...state,
+        displayedMovies: state.movies.filter(movie =>
+          movie.title.toLowerCase().includes(action.payload.toLowerCase())
+        )
       };
     default:
       return state;
