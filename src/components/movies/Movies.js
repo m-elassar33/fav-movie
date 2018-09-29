8752ee07f711b27be47fdd1078a5c9c53d351526
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-09-29 16:08:45 +0200

    Create a Movies component & connect to redux

---
+++ src/components/movies/Movies.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Movie from './Movie';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getMovies } from '../../actions/movieActions';
+
+class Movies extends Component {
+  componentDidMount() {
+    this.props.getMovies();
+  }
+
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <div className="row">
+        {movies.map(movie => (
+          <div key={movie.id} className="col-md-4">
+            <Movie movie={movie} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+Movies.propTypes = {
+  movies: PropTypes.array.isRequired,
+  getMovies: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  movies: state.movie.movies
+});
+
+export default connect(
+  mapStateToProps,
+  { getMovies }
+)(Movies);
