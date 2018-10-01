233f793f5abb0a4e14db605ae3f96c3f7adf173c
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-10-01 17:36:37 +0200

    Fix some bugs & do some refactoring

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -2,9 +2,12 @@ import { GET_MOVIES, GET_MOVIE, FILTER_MOVIES, SET_MODAL } from './types';
 import axios from 'axios';
 
 export const getMovies = () => async dispatch => {
-  const res = await axios.get(
-    'https://api.themoviedb.org/3/movie/popular?api_key=bbce44df3ab7afa8c2f1f2f6fe93b640'
-  );
+  const res = await axios.get('https://api.themoviedb.org/3/movie/popular', {
+    params: {
+      api_key: 'bbce44df3ab7afa8c2f1f2f6fe93b640',
+      region: 'US'
+    }
+  });
   dispatch({
     type: GET_MOVIES,
     payload: res.data.results
@@ -12,25 +15,27 @@ export const getMovies = () => async dispatch => {
 };
 
 export const getMovie = id => async dispatch => {
-  const res = await axios.get(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=bbce44df3ab7afa8c2f1f2f6fe93b640`
-  );
+  const res = await axios.get(`https://api.themoviedb.org/3/movie/${id}`, {
+    params: {
+      api_key: 'bbce44df3ab7afa8c2f1f2f6fe93b640'
+    }
+  });
   dispatch({
     type: GET_MOVIE,
     payload: res.data
   });
 };
 
-export const filterMovies = term => async dispatch => {
-  dispatch({
+export const filterMovies = term => {
+  return {
     type: FILTER_MOVIES,
     payload: term
-  });
+  };
 };
 
-export const setModal = modal => async dispatch => {
-  dispatch({
+export const setModal = modal => {
+  return {
     type: SET_MODAL,
     payload: modal
-  });
+  };
 };
